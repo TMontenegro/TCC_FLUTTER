9e3df1be4b9953c42f541573d62aab944aa10f5b
Tales Montenegro <montenegro.tales@gmail.com>
2019-12-09 01:36:12 -0300

    saving data on firebase

--- agoravai/lib/main.dart
+++ agoravai/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -20,7 +22,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   File _image;
   Position _position;
-  
+
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
 
@@ -30,10 +32,29 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future getLocation() async {
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
     setState(() {
       _position = position;
+    });
+  }
+
+  Future addData() async {
+    final FirebaseApp app = await FirebaseApp.configure(
+      name: 'TCCFLUTTER',
+      options: const FirebaseOptions(
+        googleAppID: 'tccflutter-b11d5',
+        gcmSenderID: '1:473033131343:android:0e65766316277b507ab13a',
+        apiKey: 'AIzaSyC4EVwHjAGT9AUTAsiNnm5ruRT9Rj1nKa4',
+        projectID: 'tccflutter-b11d5',
+      ),
+    );
+    final Firestore firestore = Firestore(app: app);
+
+    await firestore.collection("formData").add(<String, dynamic>{
+      "img": _image.toString(),
+      "coordinates": _position.toString(),
     });
   }
 
@@ -66,10 +87,19 @@ class _MyAppState extends State<MyApp> {
                   getLocation();
                 },
               ),
-               SizedBox(
+              SizedBox(
                 height: 10.0,
               ),
-               _position == null ? Container() : Text('Location: $_position'),
+              _position == null ? Container() : Text('Localização: $_position'),
+              SizedBox(
+                height: 30.0,
+              ),
+              FlatButton(
+                child: Text("Salvar"),
+                onPressed: () {
+                  addData();
+                },
+              ),
             ],
           ),
         ),
